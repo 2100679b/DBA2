db8051472ff2d00fe7fd420164a5f7b11c405a97
2100679b <2100679b@umich.mx>
2025-06-24 23:51:30 -0600

    Base de Datos II

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Si la variable de entorno está definida, úsala, si no, usa '/api' para que Netlify haga el proxy
-  baseURL: import.meta.env.VITE_API_URL || '/api',
-  timeout: 10000, // Tiempo máximo de espera en milisegundos
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3001/api',
+  timeout: 10000,
 });
 
 export default api;
