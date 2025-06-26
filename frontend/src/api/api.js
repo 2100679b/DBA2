47c94e5aa84525567923124adc6924be62979b09
2100679b <2100679b@umich.mx>
2025-06-25 20:14:03 -0600

    Base de Datos II

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV ? 'http://localhost:3000' : '',
+  baseURL: import.meta.env.VITE_API_URL || 'http://18.119.167.171:3001/api',
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  timeout: 10000 // 10 segundos de espera
 });
 
 export default api;
