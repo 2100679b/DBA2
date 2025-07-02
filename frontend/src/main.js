5db75751d152ff4d0b03646c00bc901ee213ee08
2100679b <2100679b@umich.mx>
2025-07-01 19:51:02 -0600

    Base de Datos II

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,14 +6,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css'; // Importa los estilos
+
 import '@popperjs/core'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
+app.use(Toast);
 app.use(store)
 app.use(router)
 app.mount('#app')
-
-
